2854d910b065ac26f2bbac5c3b40c1925382d9bb
minhmau <minhmau@users.noreply.github.com>
2026-03-29 19:01:34 -0700

    fix: use exact Google Maps location for Bao Bi Cong Minh factory

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -45,5 +45,5 @@ export const contactInfo: ContactInfo = {
   taxCode: "0100784051",
   zalo: "https://zalo.me/0813086886",
   mapEmbedUrl:
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3719.8!2d105.7636!3d21.1367!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135007de8a29c43%3A0x8d7f0c8b1e8a5d5e!2sKCN%20Quang%20Minh!5e0!3m2!1svi!2svn!4v1711756800000",
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3718.5!2d105.7600497!3d21.2007137!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3134fe0a962ebdc3%3A0xf183e738aaccb018!2sDoanh%20nghi%E1%BB%87p%20t%C6%B0%20nh%C3%A2n%20X%C3%AD%20Nghi%E1%BB%87p%20Bao%20B%C3%AC%20C%C3%B4ng%20Minh!5e0!3m2!1svi!2svn!4v1711756800000",
 };

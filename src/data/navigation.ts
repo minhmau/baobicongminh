028dfe0b49cac19baa53ce15548ba42cb0a44efc
minhmau <minhmau@users.noreply.github.com>
2026-03-29 18:57:19 -0700

    fix: correct Google Maps embed to KCN Quang Minh location

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -45,5 +45,5 @@ export const contactInfo: ContactInfo = {
   taxCode: "0100784051",
   zalo: "https://zalo.me/0813086886",
   mapEmbedUrl:
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.5!2d105.7800!3d21.0500!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjHCsDAzJzAwLjAiTiAxMDXCsDQ2JzQ4LjAiRQ!5e0!3m2!1sen!2svn!4v1234567890",
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3719.8!2d105.7636!3d21.1367!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135007de8a29c43%3A0x8d7f0c8b1e8a5d5e!2sKCN%20Quang%20Minh!5e0!3m2!1svi!2svn!4v1711756800000",
 };
